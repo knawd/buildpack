57117bde2e5eaac318bd2ba284237a6e2569e7ad
Anthony Whalley <anton@venshare.com>
2023-01-18 09:54:22 +0000

    feature: initial build of a wasm application
    Signed-off-by: Anthony Whalley <anton@venshare.com>

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,29 +1,17 @@
-# buildpack
+#! /bin/sh 
 
-Currently work in progress as we are testing the end to end UX but this project should build and the sample application.
-The deployment aspects are currently being investigated.
-
-## package rust builder
-```
 pack builder create quay.io/knawd/builder:rust --config ./builders/rust/builder.toml
 podman push quay.io/knawd/builder:rust
-```
 
-## package
-
-```bash
 pack buildpack package knawd-rust --config ./buildpacks/rust/package.toml
-```
 
 ## build stack
 
-```bash
 podman build . -t quay.io/knawd/stack:rust
 podman push quay.io/knawd/stack:rust
-```
-
 ## build app
 
-```
 pack build helloworld-rust-wasi --path ./apps/rust/helloworld-rust-wasi --buildpack ./buildpacks/rust --builder  quay.io/knawd/builder:rust --docker-host=inherit -v --trust-builder
-```
+
+
+
